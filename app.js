90cad6349f6a9a9aa3a8c6d227b72c14f5ddb7a6
Carlos De Oliveira <carlos.deoliveira@imsmaxims.com>
2018-07-03 11:48:30 +0100

    Included device rotation

--- app.js
+++ app.js
@@ -63,15 +63,14 @@ function crc16(s) { // crc16 fast version
     return ((crc ^ 0) & 0xFFFF);
 }
 
-window.checkMobile = function () {
-    // device detection
-    return (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
-        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4)));
+// device detection
+function checkMobile() {    
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);    
 };
 
 var Matrix = function (sizeX,sizeY) {
     
-    this.mtc = {};
+    this.mtc = {}; // coordinates in O(1)
     var i, j, matrixChars = "#£&%R38@0€".split("");    
     var maxStep = 6;
 
@@ -254,7 +253,7 @@ var ASCIIMirror = function () {
     this.resizeHandler = function(event) {        
         var mw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
         var mh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        display.height = Math.ceil(Math.min(mw, mh) * 0.9);                
+        display.height = Math.ceil(Math.min(mw, mh) * (checkMobile() ? 1 : 0.9));                
         fontsize = display.height / terminal.height;     
         display.width = Math.ceil(terminal.width * fontsize);
         container.style.width = display.width + "px";
@@ -306,7 +305,7 @@ var ASCIIMirror = function () {
         var cx = (p.x * fontsize) + (fontsize / 2); 
         var cy = ((p.y * fontsize) + (fontsize / 2));
         
-        foreColor = 'rgba(' + p.lum + ',' + p.lum + ',' + p.lum + ',' + this.Alpha + ')'; // default
+        foreColor = 'rgba(' + p.lum + ',' + p.lum + ',' + p.lum + ',' + this.Alpha + ')';
         bgColor = "#FFFFFF";
 
         if (this.displayMode == DISPLAY_MODE.Matrix) {         
@@ -317,7 +316,6 @@ var ASCIIMirror = function () {
             mc = matrix.get(p.x / 3, p.y);            
             if (p.x % 3 == 0 && mc !== null) {
                 c = mc;
-                //img_cache[cKey] = 0;
                 if (mc == "*") {                                        
                     foreColor = "#00FF00";
                     c = matrix.getChar();                    
@@ -328,7 +326,6 @@ var ASCIIMirror = function () {
                 }   
             }                               
         }
-
         
         if (this.displayMode == DISPLAY_MODE.BackColor) {
             foreColor = bgColor;
@@ -356,20 +353,16 @@ var ASCIIMirror = function () {
             c = ci;
         }
 
-        cKey = crc16([c, foreColor, bgColor].join("."));
-        if (img_cache[cKey]) {
-            img_cache[cKey]--;
-            return false;
-        }
-        img_cache[cKey] = 5;
+        cKey = crc16([c, foreColor, bgColor].join(".")); 
+
+        if (img_cache[cKey]) return --img_cache[cKey]; else img_cache[cKey] = 5;
 
         if (lastStyle != this.displayMode) {
-            img_cache = {};
+            for(var k in img_cache) delete img_cache[k]; // better than empty the object? 
             lastStyle = this.displayMode;
             g_ctx.fillStyle = bgColor;
             g_ctx.fillRect(0, 0, display.width, display.height);
         }
-
 
         g_ctx.fillStyle = bgColor;
         g_ctx.fillRect(p.x * fontsize, p.y * fontsize, fontsize, fontsize); 
@@ -478,6 +471,7 @@ var ASCIIMirror = function () {
                 return;
             });
         } else {
+            // fallback other browsers
             try {
                 navigator.getUserMedia =
                     navigator.getUserMedia ||
@@ -505,7 +499,12 @@ var ASCIIMirror = function () {
         setTimeout(function() {
             if(divWarning) document.body.appendChild(divWarning);
         }, 5000);
+        // resize events
         window.addEventListener('resize', self.resizeHandler, true);        
+        if("onorientationchange" in window) {
+            window.addEventListener('onorientationchange', self.resizeHandler, false);        
+        };
+
     }
 
     this.onFpsChange = function(callback) {
